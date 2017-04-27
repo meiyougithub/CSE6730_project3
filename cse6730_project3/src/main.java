b4640eac77c6fe23a70ca35ed917f71ca5b5270b
meiyougithub <xianghui@umich.edu>
2017-04-27 19:05:47 -0400

    Add files via upload

---
+++ cse6730_project3/src/main.java
@@ -0,0 +1,41 @@
+import java.util.*;
+
+
+class VirusSim {
+
+    public static void main(String[] args){
+        int clock = 0;
+        int tick = 1;
+        int maxLength = 4 * 7 * 24;
+        int numCities = 50;
+        City[] cities = new Cities[numCities];
+        TreeSet<TransportEvent> events = new TreeSet<>();
+        /*
+
+        Initialize cities here
+
+        */
+        while (clock < maxLength){
+            if (!events.isEmpty()){
+                TransportEvent tempEvent = events.first();
+                while (tempEvent.getDestTime() == clock){
+                    events.remove();
+                    int tempPop = tempEvent.getNumPassenger();
+                    City tempCity = tempEvent.getDestCity();
+                    tempCity.setPopWithoutAntibody() = tempCity.getPopWithoutAntibody() + tempPop;
+                    tempEvent = events.first();
+
+            }
+
+            for (int i = 0; i < numCities; i++){
+                Cities[i].hospitalTurn();
+                Cities[i].virusTurn();
+                Cities[i].transportTurn(clock, events);
+
+            }
+        }
+        clock ++;
+
+    }
+
+}
---
+++ cse6730_project3/src/transportEvent.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+class TransportEvent implements Comparable<TransportEvent>{
+	
+	private int destTime;
+	private int numPassenger;
+	private City destCity;
+
+	TransportEvent(int destTime, int numPassenger, City destCity){
+		this.destTime = destTime;
+		this.numPassenger = numPassenger;
+		this.destCity = destCity;
+	}
+
+    public int compareTo(final TransportEvent ev){
+    	return this.getDestTime() - ev.getDestTime();
+    } 
+
+    public int getDestTime(){return this.destTime;}
+    public City getDestCity(){return this.destCity;}
+    public int getNumPassenger(){return this.numPassenger;}
+
+
+}
