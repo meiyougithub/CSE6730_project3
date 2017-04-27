94c26047c6c62417a013ec1c55dbfca2c26895d7
meiyougithub <xianghui@umich.edu>
2017-04-27 16:46:30 -0400

    Add files via upload

---
+++ cse6730_project3/src/transportEvent.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+class TransportEvent implements Comparable<TransportEvent>{
+	
+	private int destTime;
+	private int numPassenger;
+	private City destCity;
+
+	TransportEvent(int destTime, int numPassenger, City destCity){
+		this.destTime = destTime;
+		this.numPassenger = numPassenger;
+		this.destCity = destCity;
+	}
+
+    public int compareTo(final TransportEvent ev){
+    	return this.getDestTime() - ev.getDestTime();
+    } 
+
+    int getDestTime(){return this.destTime;}
+
+
+}
