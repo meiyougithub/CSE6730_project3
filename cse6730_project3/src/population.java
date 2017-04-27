6309d8b6a48c234cc14c8f3f3ec9b0ea2aa6bb83
meiyougithub <xianghui@umich.edu>
2017-04-27 15:31:46 -0400

    Update population.java
    add comments

--- cse6730_project3/src/population.java
+++ cse6730_project3/src/population.java
@@ -2,32 +2,43 @@
  * Created by WYF on 4/27/2017.
  */
 public class population {
-    private int p_total_pop;
-    private int p_pop_antibody;
-    private int p_pop_withoutantibody;
-    private int p_pop_dead;
-    private int p_pop_symptom;
-    private int p_pop_infected;
+    private int total_pop; // total population 
+    private int pop_antibody; // healthy people with antibody
+    private int pop_withoutantibody; // healthy people without antibody
+    private int pop_dead; // dead people caused by virus
+    private int pop_symptom; // infected people who has symptoms
+    private int pop_infected; // infected people who don't display symptoms
+    private int pop_quarantine; // quarantined pop_symptom in hospital
+    private double antibody_ratio = 0.001;
+    
+    public population(int total_pop){
+        total_pop = total_pop;
+        pop_antibody = (int) total_pop * antibody_ratio;
+        pop_withoutantibody = total_pop - pop_antibody;
+        pop_dead = 0;
+        pop_symptom = 0;
+        pop_infected = 0;
+    }
+    
     public population(int total_pop,int pop_antibody, int pop_withoutantibody, int pop_dead, int pop_symptom, int pop_infected){
-        p_total_pop=total_pop;
-        p_pop_antibody=pop_antibody;
-        p_pop_withoutantibody=pop_withoutantibody;
-        p_pop_dead=pop_dead;
-        p_pop_symptom=pop_symptom;
-        p_pop_infected=pop_infected;
+        total_pop = total_pop;
+        pop_antibody = pop_antibody;
+        pop_withoutantibody = pop_withoutantibody;
+        pop_dead = pop_dead;
+        pop_symptom = pop_symptom;
+        pop_infected = pop_infected;
     }
 
-    public int getP_total_pop() {return p_total_pop;}
-    public int getP_pop_antibody() {return p_pop_antibody;}
-    public int getP_pop_withoutantibody() {return p_pop_withoutantibody;}
-    public int getP_pop_dead() {return p_pop_dead;}
-    public int getP_pop_infected() {return p_pop_infected;}
-    public int getP_pop_symptom() {return p_pop_symptom;}
-    public void setP_pop_antibody(int p_pop_antibody) {this.p_pop_antibody = p_pop_antibody;}
-    public void setP_pop_dead(int p_pop_dead) {this.p_pop_dead = p_pop_dead;}
-    public void setP_pop_infected(int p_pop_infected) {this.p_pop_infected = p_pop_infected;}
-    public void setP_pop_symptom(int p_pop_symptom) {this.p_pop_symptom = p_pop_symptom;}
-    public void setP_pop_withoutantibody(int p_pop_withoutantibody) {this.p_pop_withoutantibody = p_pop_withoutantibody;}
-    public void setP_total_pop(int p_total_pop) {this.p_total_pop = p_total_pop;}
+    public int getTotal_pop() {return total_pop;}
+    public int getPop_antibody() {return pop_antibody;}
+    public int getPop_withoutantibody() {return pop_withoutantibody;}
+    public int getPop_dead() {return pop_dead;}
+    public int getPop_infected() {return pop_infected;}
+    public int getPop_symptom() {return pop_symptom;}
+    public void setPop_antibody(int pop_antibody) {this.pop_antibody = pop_antibody;}
+    public void setPop_dead(int pop_dead) {this.pop_dead = pop_dead;}
+    public void setPop_infected(int pop_infected) {this.pop_infected = pop_infected;}
+    public void setPop_symptom(int pop_symptom) {this.pop_symptom = pop_symptom;}
+    public void setPop_withoutantibody(int pop_withoutantibody) {this.pop_withoutantibody = pop_withoutantibody;}
+    public void setTotal_pop(int total_pop) {this.total_pop = total_pop;}
 }
-
